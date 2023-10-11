b73aca11f115be063ad7899f797aa6032b2323f4
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2023-10-11 17:06:32 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(arr, stri) {
+    return arr.filter(function(num) {
+        return num.toLowerCase() === stri.toLowerCase();
+    });
+}
+function fuzzyMatch(arr, stri){
+    return arr.filter(function(num){
+        // console.log()
+        // console.log(num[0,stri.length])
+        // console.log(stri)
+        return num.slice(0,2) === stri
+    })}
+
+// }fuzzyMatch(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'],'Sa')
+function matchName(arr, stri) {
+    return arr.filter(function(name) {
+        return name.name.toLowerCase() === stri.toLowerCase();
+    });
+}
